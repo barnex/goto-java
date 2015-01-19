3d9347f9844be9265b27e0f830d2bafdc0a6c9f2
Arne Vansteenkiste <barnex@gmail.com>
2015-01-19 22:00:55 +0100

    generate formatted code

--- writer.go
+++ writer.go
@@ -12,37 +12,41 @@ import (
 
 const (
 	CLASS   = "class"
+	EOL     = ";"
 	FINAL   = "final"
+	LBRACE  = "{"
 	PACKAGE = "package"
 	PUBLIC  = "public"
+	RBRACE  = "}"
+	STATIC  = "static"
+	VOID    = "void"
 )
 
 type writer struct {
-	fset      *token.FileSet
-	fname     string // output file name for principal class (no extension)
-	pkg       string
-	out       io.Writer
-	classDecl []*ast.Decl
+	fset         *token.FileSet
+	fname        string // output file name for principal class (no extension)
+	pkg          string
+	out          io.Writer
+	classMembers []ast.Decl
+	indent       int
 }
+
+// parsing
 
 func (w *writer) parseFile(f *ast.File) {
 	w.pkg = f.Name.Name
 
 	for _, decl := range f.Decls {
-		switch decl.(type) {
+		switch n := decl.(type) {
 		default:
 			w.error(f, "cannot handle ", reflect.TypeOf(decl))
+		case *ast.FuncDecl:
+			w.classMembers = append(w.classMembers, n)
 		}
 	}
 }
 
-func (w *writer) error(n ast.Node, msg ...interface{}) {
-	fatal(append([]interface{}{w.pos(n), ": "}, msg...)...)
-}
-
-func (w *writer) pos(n ast.Node) token.Position {
-	return w.fset.Position(n.Pos())
-}
+// code gen
 
 func (w *writer) initOut() {
 	if w.out != nil {
@@ -57,19 +61,56 @@ func (w *writer) initOut() {
 func (w *writer) genCode() {
 	w.initOut()
 
-	w.putln(PACKAGE, w.pkg)
-	w.println()
+	w.putln(PACKAGE, w.pkg, EOL)
 
 	className := path.Base(w.fname)
-	w.put(PUBLIC, FINAL, CLASS, className, "{\n")
+	w.open(PUBLIC, FINAL, CLASS, className)
+	w.putln()
 
-	w.genClassDecls()
+	w.genMembers()
 
-	w.put("}\n")
+	w.close()
 }
 
-func (w *writer) genClassDecls() {
+func (w *writer) genMembers() {
+	for _, n := range w.classMembers {
+		switch m := n.(type) {
+		default:
+			panic("unhandled memeber type: " + reflect.TypeOf(m).String())
+		case *ast.FuncDecl:
+			w.genFuncDecl(m)
+		}
+	}
+}
 
+func (w *writer) genFuncDecl(n *ast.FuncDecl) {
+	if n.Name.Name == "main" {
+		w.genMainDecl(n)
+		return
+	}
+
+	panic("todo")
+}
+
+func (w *writer) genMainDecl(n *ast.FuncDecl) {
+	w.open(PUBLIC, STATIC, FINAL, VOID, n.Name.Name)
+	w.close()
+	w.putln()
+}
+
+func (w *writer) putln(tokens ...interface{}) {
+	w.putIndent()
+	w.put(append(tokens, "\n")...)
+}
+
+func (w *writer) open(tokens ...interface{}) {
+	w.putln(append(tokens, LBRACE)...)
+	w.indent++
+}
+
+func (w *writer) close() {
+	w.indent--
+	w.putln(RBRACE)
 }
 
 func (w *writer) put(tokens ...interface{}) {
@@ -81,12 +122,18 @@ func (w *writer) put(tokens ...interface{}) {
 	}
 }
 
-func (w *writer) putln(tokens ...interface{}) {
-	w.put(tokens...)
-	w.put(";\n")
+func (w *writer) putIndent() {
+	for i := 0; i < w.indent; i++ {
+		fmt.Fprint(w.out, "\t")
+	}
 }
 
-func (w *writer) println(x ...interface{}) {
-	w.put(x...)
-	w.put("\n")
+// exit with fatal error, print token position of node n and msg.
+func (w *writer) error(n ast.Node, msg ...interface{}) {
+	fatal(append([]interface{}{w.pos(n), ": "}, msg...)...)
+}
+
+// return position of node using this writer's fileset
+func (w *writer) pos(n ast.Node) token.Position {
+	return w.fset.Position(n.Pos())
 }
