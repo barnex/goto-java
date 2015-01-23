f3c7aceecb8c1d514aac866117b4ef2cd32cb78a
Arne Vansteenkiste <barnex@gmail.com>
2015-01-23 20:39:52 +0100

    add globals test (failing)

---
+++ test/main/globals.go
@@ -0,0 +1,48 @@
+package main
+
+var a int
+var b int = 1
+var c = 2
+var d, e = 3, 4
+
+var (
+	f    int
+	g    int = 5
+	h        = 6
+	i, j     = 7, 8
+)
+
+func main() {
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(f)
+	println(g)
+	println(h)
+	println(i)
+	println(j)
+	println(A)
+	println(B)
+	println(C)
+	println(D)
+	println(E)
+	println(F)
+	println(G)
+	println(H)
+	println(I)
+	println(J)
+}
+
+var A int
+var B int = 10
+var C = 20
+var D, E = 30, 40
+
+var (
+	F    int
+	G    int = 50
+	H        = 60
+	I, J     = 70, 80
+)
--- test/main/scope1.go
+++ test/main/scope1.go
@@ -1,10 +1,19 @@
 package main
 
+var (
+	i = 47
+	j = 48
+)
+
 func main() {
 	// test scope
 	i := 1
 	{
 		i := 2
+		{
+			i := 5
+			println(i)
+		}
 		println(i)
 	}
 	println(i)
@@ -16,4 +25,10 @@ func main() {
 
 	j := 4
 	println(j)
+
+	println(getI())
+	println(getJ())
 }
+
+func getI() int { return i }
+func getJ() int { return j }
