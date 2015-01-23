31289cca242bea9420842c47da14fbb71c1bf7d9
Arne Vansteenkiste <barnex@gmail.com>
2015-01-23 21:14:30 +0100

    add consts test

---
+++ test/main/consts.go
@@ -0,0 +1,40 @@
+package main
+
+const b int = 1
+const c = 2
+const d, e = 3, 4
+
+const (
+	g    int = 5
+	h        = 6
+	i, j     = 7, 8
+)
+
+func main() {
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(g)
+	println(h)
+	println(i)
+	println(j)
+	println(B)
+	println(C)
+	println(D)
+	println(E)
+	println(G)
+	println(H)
+	println(I)
+	println(J)
+}
+
+const B int = 10
+const C = 20
+const D, E = 30, 40
+
+const (
+	G    int = 50
+	H        = 60
+	I, J     = 70, 80
+)
