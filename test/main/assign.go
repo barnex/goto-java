e073251a1eda059fa6a79cb717e961c7493c7416
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-01-24 19:48:41 +0100

    more tests

---
+++ test/main/assign.go
@@ -0,0 +1,18 @@
+package main
+
+func main(){
+	var a, b, c, d int = 1, 2, 3, 4
+
+	a = 5
+	a += b
+	b -= c
+	b *= c
+	b /= c
+	d %= 3
+	b ^= c
+
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+}
--- test/main/vars2.go
+++ test/main/vars2.go
@@ -4,7 +4,7 @@ func main(){
 	
 	a:=1
 	a, b := 2, 3	
-	c, a , b := 4, 5, 6
+	c, a, b := 4, 5, 6
 
 	println(a)
 	println(b)
