d17ffcf8208f6e821d3ba6344f5a8ec3d3c74293
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-01-24 20:45:10 +0100

    cleanup zerovalues.go

--- init.go
+++ init.go
@@ -1,17 +1,21 @@
 package main
 
-var zeroValues = map[string]string{
-	"String":  `""`,
-	"int":     "0",
-	"float":   "0.0f",
-	"double":  "0.0",
-	"boolean": "false",
-}
 
+// ZeroValue returns the zero value for a new variable of java type jType.
+// E.g.:
+// 	var x int  ->  int x = 0;
 func ZeroValue(jType string) string {
 	if v, ok := zeroValues[jType]; ok {
 		return v
 	} else {
 		return "null"
 	}
+}
+
+var zeroValues = map[string]string{
+	"String":  `""`,
+	"int":     "0",
+	"float":   "0.0f",
+	"double":  "0.0",
+	"boolean": "false",
 }
