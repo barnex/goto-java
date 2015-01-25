af174a97dd23618b21ddf3b479faa97a6daeee90
Arne Vansteenkiste <barnex@gmail.com>
2015-01-25 20:32:27 +0100

    add void test

--- test/main/statics.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	static := 1
-	println(static)
-
-	private := 2
-}
---
+++ test/main/void.go
@@ -0,0 +1,10 @@
+package main
+
+// test function with void return type
+func f() {
+	println(42)
+}
+
+func main() {
+	f()
+}
