d1f7b841021123b9403f9736ec7f5c1c13bd843d
Arne Vansteenkiste <barnex@gmail.com>
2015-02-01 18:16:24 +0100

    start doc.go

---
+++ doc.go
@@ -0,0 +1,38 @@
+/*
+goto-java is a Go to Java transpiler.
+
+
+
+
+Formatting
+
+...
+
+
+Commentary
+
+
+Names
+
+*File/class names
+*Package names
+*interface/struct names
+*renaming
+*caps
+
+
+Control structures
+
+
+
+
+
+
+
+Doc comments are transferred to java unchanged.
+TODO: slash-star-star
+TODO: Other comments are not.
+
+
+*/
+package main
