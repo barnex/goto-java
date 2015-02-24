50f3cd7191274c2879728003d2537074c23cb60f
Arne Vansteenkiste <barnex@gmail.com>
2015-02-24 23:14:43 +0100

    merge typeinference, vars tests

--- test/vars.go
+++ test/vars.go
@@ -1,6 +1,7 @@
 package main
 
-// test variable declarations, type inference and type conversion
+// test variable declarations and type inference
+
 func main() {
 
 	var a int           // no init value
@@ -9,6 +10,14 @@ func main() {
 	var e = 4           // var, type inferred from value
 	var f, g = 5, "hi"  // var, two different types
 	var h string        // var, string, no init value
+
+	A := 1
+	B, C := A, 2+3
+	F := F()
+	println(A)
+	println(B)
+	println(C)
+	println(F)
 
 	var (
 		i    int
@@ -30,4 +39,32 @@ func main() {
 	println(k)
 	println(l)
 	println(m)
+
+	testShortRedefine()
 }
+
+var b = 42 // try to confuse scope
+
+func F() int { return 56 }
+
+// short variable re-declaration
+func testShortRedefine() {
+	{
+		// try to confuse scope
+		a, b, c := 999, 999, 999
+		println(a)
+		println(b)
+		println(c)
+	}
+
+	a := 1
+	a, b := 2, 3
+	c, a, b := 4, 5, 6
+
+	println(a)
+	println(b)
+	println(c)
+
+}
+
+var c = 43 // try to confuse position info
