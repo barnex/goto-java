e2c19e82c9c8e6656b33d7e215c974baf7caf0d8
Arne Vansteenkiste <barnex@gmail.com>
2015-02-24 20:22:04 +0100

    merge blankident*.go tests

--- test/blankident.go
+++ test/blankident.go
@@ -1,14 +1,36 @@
 package main
 
+// Test blank identifier (_)
+
+// Try to induce name collision with names generated for blank identifier
+// (flag -blank)
+var _ = 3
+var _0 = 4
+var _1 = 5
+
 func main() {
 	_ = f(1)
 	_ = f(1)
 	(_) = f(1)
+	(_) = 2
 	_, i := f(2), f(3)
 	println(i)
+
+	println(f2(1, 2))
 }
 
 func f(x int) int {
 	println(x)
 	return x + 1
+}
+
+func f2(_ int, _ int) int {
+	var _ = 3
+	var _0 = 4
+	var _1 = 5
+	var _2 = 6
+	println(_0)
+	println(_1)
+	println(_2)
+	return 42
 }
--- test/blankident2.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func f(_ int, _ int) int {
-	return 42
-}
-
-func main() {
-	println(f(1, 2))
-}
