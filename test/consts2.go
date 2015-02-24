2923596bc1dc30b43590a337e1d913714be716c5
Arne Vansteenkiste <barnex@gmail.com>
2015-02-24 22:43:39 +0100

    move some tests to todo/

--- test/consts2.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const big = 1 << 33
-
-func main() {
-	println(big)
-}
--- test/todo/bigints.go
+++ test/todo/bigints.go
@@ -1,5 +1,14 @@
 package main
 
+const (
+	big1 = 1 << 31
+	big2 = 1 << 32
+	big3 = 1 << 33
+	big4 = 1 >> 31
+	big5 = 1 >> 32
+	big6 = 1 >> 33
+)
+
 func main() {
 	println(1 << 31)
 	println(1 << 32)
@@ -7,4 +16,11 @@ func main() {
 	println(1 >> 31)
 	println(1 >> 32)
 	println(1 >> 33)
+
+	println(big1)
+	println(big2)
+	println(big3)
+	println(big4)
+	println(big5)
+	println(big6)
 }
